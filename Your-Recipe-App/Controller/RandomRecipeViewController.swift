c553536ee54b84fcbea8206ae8982e26d0b637d0
drknozan <drknozan@gmail.com>
2022-12-17 19:54:08 +0300

    feat: get random recipe

--- Your-Recipe-App/Controller/RandomRecipeViewController.swift
+++ Your-Recipe-App/Controller/RandomRecipeViewController.swift
@@ -7,15 +7,33 @@
 
 import Foundation
 import UIKit
+import RealmSwift
 
 class RandomViewController: UIViewController {
     
+    let realm = try! Realm()
+    var recipes: Results<Recipe>?
     
     @IBOutlet weak var nameLabel: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        getRecipesFromDb()
     }
     
+    func getRecipesFromDb() {
+        recipes = realm.objects(Recipe.self)
+    }
+    
+    @IBAction func randomButtonPressed(_ sender: UIButton) {
+        let recipeCount = recipes?.count ?? 0
+        
+        if recipeCount == 0 {
+            nameLabel.text = "Please add recipe"
+        } else {
+            let randInt = Int.random(in: 0 ..< recipeCount)
+            nameLabel.text = recipes?[randInt].name
+        }
+    }
 }
