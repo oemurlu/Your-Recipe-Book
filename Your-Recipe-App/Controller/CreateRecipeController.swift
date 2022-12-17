2fdefe966d6b8075f4fba951ce491be611b41d98
oemurlu <oemurlu@gmail.com>
2022-12-17 19:48:50 +0300

    feat: create new recipe

--- Your-Recipe-App/Controller/CreateRecipeController.swift
+++ Your-Recipe-App/Controller/CreateRecipeController.swift
@@ -7,7 +7,7 @@
 
 import Foundation
 import UIKit
-
+import RealmSwift
 
 class CreateRecipeController: UIViewController {
 
@@ -16,10 +16,38 @@ class CreateRecipeController: UIViewController {
     @IBOutlet weak var ingredientsTextField: UITextView!
     @IBOutlet weak var recipeTextField: UITextView!
     
-    
+    let realm = try! Realm()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+    }
+
+
+    @IBAction func saveButtonPressed(_ sender: Any) {
+        
+        let recipe = Recipe()
+        
+        let name = nameTextField.text!
+        let ingredients = ingredientsTextField.text!
+        let recipeInput = recipeTextField.text!
+        
+        recipe.name = name
+        recipe.ingredients = ingredients
+        recipe.recipe = recipeInput
+        
+        
+        do {
+            try realm.write({
+                realm.add(recipe)
+                navigationController?.popViewController(animated: true)
+                
+            })
+        } catch {
+            print("error saving to realm db")
+        }
+        
+        
+        
     }
 }
